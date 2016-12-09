0e0d4c9870a4aa56f438da51bf3b17c4a07138e2
aliaksandr-s <s-a-n-9@mail.ru>
2016-12-09 12:30:43 +0300

    init

---
+++ createCashFlow.js
@@ -0,0 +1,143 @@
+db.loadServerScripts();
+createCashFlow();
+
+function createCashFlow(){
+    db.getCollection('cash_flow').remove({})
+    
+    var dates = [],
+        start_date = getFirstAndLastDates("Usd")["start_date"],
+        end_date = getFirstAndLastDates("Usd")["end_date"],
+        cash_flow = db.getCollection('cash_flow'),
+        money = {
+            "Byr": 0,
+            "Byn": 0,
+            "Usd": 0
+        };
+    
+     end_date = new Date(end_date.setTime( end_date.getTime() + 1 * 86400000 )) // take care of the last date
+    
+     // fill the array with dates for each day
+     while (start_date <= end_date) {
+        dates.push( new Date (start_date) )
+        start_date = new Date(start_date.setTime( start_date.getTime() + 1 * 86400000 )); 
+    }
+    
+    //return dates
+    
+    // for each day count spendings
+    dates.forEach(function (day){
+       
+        day.setMinutes(0);
+        day.setSeconds(0);
+        day.setMilliseconds(0);
+        day.setHours(0);
+        
+        cash_flow.insert(countSpendingsForTheDay(day, money))
+    })
+}    
+
+function getAllTransactionsForTheDay(day){
+    var copy_of_the_day = new Date(day), // make a copy to prevent side effects
+        current_day = new Date(day),
+        next_day = new Date(copy_of_the_day.setTime( copy_of_the_day.getTime() + 1 * 86400000 ));
+    
+    return db.getCollection('account_transactions').find({"Date": {
+        $gte: current_day,
+        $lt: next_day,
+    }}).toArray()
+}
+
+function countSpendingsForTheDay(day, money){
+    var transactions = getAllTransactionsForTheDay(day);
+    
+    // count all expenses and incomes for the day and update money object
+    transactions.forEach(function(transaction){
+        if (transaction["Type"] === "Exp"){
+            
+            switch(transaction["Currency"]){
+                case "Usd":
+                    money["Usd"] -= transaction["Value"];
+                    break;
+                case "Byr":
+                    money["Byr"] -= transaction["Value"];
+                    break;
+                case "Byn":
+                    money["Byn"] -= transaction["Value"];
+                    break;
+            }
+
+        } else if (transaction["Type"] === "Inc") {
+            
+            switch(transaction["Currency"]){
+                case "Usd":
+                    money["Usd"] += transaction["Value"];
+                    break;
+                case "Byr":
+                    money["Byr"] += transaction["Value"];
+                    break;
+                case "Byn":
+                    money["Byn"] += transaction["Value"];
+                    break;
+            }
+            
+        }        
+    })
+    
+    // if money gets negative and we have money to exchange. Create two exchange transactions
+    if (isExchangePossible(money)) {
+        var exchange_info = getExchangeData(day, money)
+    }
+    
+ 
+    return {
+        "Date": new Date(day),
+        "Byr": money["Byr"],
+        "Byn": money["Byn"],
+        "Usd": money["Usd"],
+        "possibility": isExchangePossible(money),
+        "exchangeInfo": isExchangePossible(money) ? exchange_info : null
+    }
+}
+
+function getExchangeData(day, money) {
+    var exchange_info = {};
+        
+    for (var prop in money) {
+        if (money[prop] < 0) {
+            exchange_info.need_to_buy = {currency: prop, value: money[prop]}
+        } else {
+            exchange_info.can_be_sold = {currency: prop, value: money[prop]}
+        }
+    }  
+
+    exchange_info.currency_rate = getCurrencyRateForTheDay(day);
+    exchange_info.need_to_sell = {};
+    exchange_info.need_to_sell.value = Math.floor(Math.abs(exchange_info.need_to_buy.value) / exchange_info.currency_rate);
+    exchange_info.need_to_sell.value = exchange_info.need_to_sell.value > exchange_info.can_be_sold.value ?
+                                       exchange_info.can_be_sold.value : exchange_info.need_to_sell.value;
+    
+    exchange_info.need_to_sell.currency = exchange_info.can_be_sold.currency
+    
+    exchange_info.date = new Date(day);
+
+    return exchange_info;
+}
+
+function getCurrencyRateForTheDay(day) {
+    var copy_of_the_day = new Date(day), // make a copy to prevent side effects
+        current_day = new Date(day),
+        next_day = new Date(copy_of_the_day.setTime( copy_of_the_day.getTime() + 1 * 86400000 ));
+    
+    return db.getCollection('currency_all').find({"date": {
+        $gte: current_day,
+        $lt: next_day,
+    }}).toArray()[0]["value"]
+}
+
+function isExchangePossible(money){
+    if ( ((money["Byr"] < 0 || money["Byn"] < 0) && money["Usd"] > 0) || ((money["Byr"] > 0 || money["Byn"] > 0) && money["Usd"] < 0) ) {
+        return true;
+    } else {
+        return false;
+    }
+}
